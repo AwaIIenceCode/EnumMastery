e3894ce4af70c695f61e793be0d096f4b20f3a54
AwallenceCode <awallencecode@gmail.com>
2025-09-26 13:20:42 +0300

    writed simple task with enum

--- EnumMastery/Program.cs
+++ EnumMastery/Program.cs
@@ -1,7 +1,30 @@
 ﻿using System;
-
 class MyClass
 {
+    enum DayOfWeek
+    {
+        Monday,
+        Tuesday, 
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+        
+    }
     static void Main()
-    {}
+    {
+        Console.WriteLine("Welcome to the calendar tracker. Please, enter the day of week!");
+
+        string userChoice = Console.ReadLine();
+
+        if (Enum.TryParse(userChoice, true, out DayOfWeek day))
+        {
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Friday) { Console.WriteLine("Work day, good luck!"); }
+        
+            else { Console.WriteLine("Chill day, bro!"); }    
+        }
+
+        else { Console.WriteLine("There is no such day of the week"); }
+    }
 }
